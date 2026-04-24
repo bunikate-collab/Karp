0a2d9286eaa631bb7dcafdb00f9117e336c20780
Ekaterina <bunikate@gmail.com>
2026-04-24 15:47:22 +0300

    [upd lesson]

--- p.py
+++ p.py
@@ -1,6 +1,16 @@
+# Задача 1
+
 true_answer = 7
 all_summ = 3.7
 
 print(true_answer, all_summ)
 
-# Задача 1
+# Задача 2
+
+age_str = "25"
+age_int = int(age_str)
+
+temperature = 30
+temperature_str = str(temperature)
+
+print(type(age_str), type(age_int), type(temperature), type(temperature_str))
